4000c24c3d87f6737b9cd361f254b84050052a4d
dakshvasudev <vasudevdaksh@gmail.com>
2024-01-26 02:23:45 +0530

    Refactor ticket widget and add tab navigation

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:ticket_booking/config/constants.dart';
 import 'package:ticket_booking/config/theme/theme.dart';
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -12,54 +12,143 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
   List<List<Color>> colorsList = const [
     [Color(0xffa9507c), Color(0xFFDB86AD)],
     [Color(0xff7b4fd0), Color(0xFFA382E6)],
     [Color(0xff275e45), Color(0xff56b295)],
     [Color(0xff987654), Color(0xfffedcba)],
     [Color(0xff112233), Color(0xff445566)],
-    // Add more lists of colors as needed
   ];
+
+  late PageController _pageController;
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController(initialPage: 0);
+    _tabController = TabController(length: 2, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    _tabController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: kBackgroundColor,
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(8, 52, 8, 24),
-        child: Column(
-          children: [
-            Text(
-              'Your tickets',
-              style: typography(context).title1,
-            ),
-            const SizedBox(
-              height: 16,
-            ),
-            Expanded(
-              child: Stack(
-                children: List.generate(
-                  3,
-                  (index) {
-                    return Positioned(
-                      top: index * 82,
-                      child: TicketWidget(
-                        margin: const EdgeInsets.fromLTRB(0, 8, 0, 8),
-                        width: MediaQuery.of(context).size.width - 16,
-                        height: 200,
-                        isCornerRounded: true,
-                        colorsList: colorsList[index % colorsList.length],
-                        color: Colors.blue,
-                        child: TicketData(
-                          singerInfo: singerInfoList[index],
-                        ),
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        backgroundColor: kBackgroundColor,
+        body: Padding(
+          padding: const EdgeInsets.fromLTRB(8, 52, 8, 24),
+          child: Column(
+            children: [
+              Text(
+                'Your tickets',
+                style: typography(context).title1,
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      _pageController.animateToPage(0,
+                          duration: const Duration(milliseconds: 300),
+                          curve: Curves.easeInOut);
+                      setState(() {
+                        _tabController.index = 0;
+                      });
+                    },
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: _tabController.index == 0
+                            ? Colors.red
+                            : Colors.white12,
+                        borderRadius: BorderRadius.circular(8),
                       ),
-                    );
+                      padding: const EdgeInsets.all(8),
+                      child: Text('Upcoming',
+                          style: typography(context).smallBody),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 8,
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      _pageController.animateToPage(1,
+                          duration: const Duration(milliseconds: 300),
+                          curve: Curves.easeInOut);
+                      setState(() {
+                        _tabController.index = 1;
+                      });
+                    },
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: _tabController.index == 1
+                            ? Colors.red
+                            : Colors.white12,
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      padding: const EdgeInsets.all(8),
+                      child:
+                          Text('History', style: typography(context).smallBody),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              Expanded(
+                child: PageView(
+                  controller: _pageController,
+                  onPageChanged: (index) {
+                    _tabController.animateTo(index);
                   },
+                  children: [
+                    // Upcoming Tab
+                    Stack(
+                      children: List.generate(
+                        3,
+                        (index) {
+                          return Positioned(
+                            top: index * 82,
+                            child: TicketWidget(
+                              margin: const EdgeInsets.fromLTRB(0, 8, 0, 8),
+                              width: MediaQuery.of(context).size.width - 16,
+                              height: 200,
+                              isCornerRounded: true,
+                              colorsList: colorsList[index % colorsList.length],
+                              color: Colors.blue,
+                              child: TicketData(
+                                singerInfo: singerInfoList[index],
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                    // History Tab (You can replace this with your history implementation)
+                    Center(
+                      child: Text(
+                        'History Tab Content',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ],
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
