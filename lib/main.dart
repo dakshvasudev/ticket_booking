ba450886cb6edac27bce4d8c04a4a8e1f9d814c5
dakshvasudev <vasudevdaksh@gmail.com>
2024-01-26 01:14:06 +0530

    Add persistent bottom navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:persistent_bottom_nav_bar/persistent_tab_view.dart';
+import 'package:ticket_booking/config/constants.dart';
+import 'package:ticket_booking/pages/choose_location.dart';
 import 'package:ticket_booking/pages/home_page.dart';
 
 void main() {
   runApp(const MyApp());
 }
+
+PersistentTabController _controller = PersistentTabController(initialIndex: 0);
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -11,13 +16,60 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Ticket Assignment',
+      title: 'Ticket Booking',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        primarySwatch: Colors.blue,
         useMaterial3: true,
       ),
-      home: const HomePage(),
+      home: PersistentTabView(
+        context,
+        controller: _controller,
+        screens: _buildScreens(),
+        items: _navBarsItems(),
+        confineInSafeArea: true,
+        backgroundColor: kBackgroundColor,
+        handleAndroidBackButtonPress: true,
+        resizeToAvoidBottomInset: true,
+        stateManagement: true,
+        hideNavigationBarWhenKeyboardShows: true,
+        decoration: NavBarDecoration(
+          borderRadius: BorderRadius.circular(10.0),
+          colorBehindNavBar: kBackgroundColor,
+        ),
+        popAllScreensOnTapOfSelectedTab: true,
+        popActionScreens: PopActionScreensType.all,
+        itemAnimationProperties: const ItemAnimationProperties(
+          duration: Duration(milliseconds: 200),
+          curve: Curves.ease,
+        ),
+        screenTransitionAnimation: const ScreenTransitionAnimation(
+          animateTabTransition: true,
+          curve: Curves.ease,
+          duration: Duration(milliseconds: 200),
+        ),
+        navBarStyle: NavBarStyle.style13,
+      ),
     );
   }
+}
+
+List<Widget> _buildScreens() {
+  return [HomePage(), ChooseLocationPage()];
+}
+
+List<PersistentBottomNavBarItem> _navBarsItems() {
+  return [
+    PersistentBottomNavBarItem(
+      icon: const Icon(Icons.home),
+      title: ("Home"),
+      activeColorPrimary: Colors.red,
+      inactiveColorPrimary: Colors.grey,
+    ),
+    PersistentBottomNavBarItem(
+      icon: const Icon(Icons.search),
+      title: ("Search Location"),
+      activeColorPrimary: Colors.red,
+      inactiveColorPrimary: Colors.grey,
+    ),
+  ];
 }
